6995d2209be399634a6e39c440753241ede18088
yizzuide <fu837014586@163.com>
2022-11-27 02:57:31 +0800

    refactor(page-list): refactor page list with page proxy handler

--- src/components/system/user/UserListPage.vue
+++ src/components/system/user/UserListPage.vue
@@ -4,9 +4,9 @@
       :page="userPageConfig"
       :page-count="usersStore.pageCount"
       :total="usersStore.totalSize"
-      @search="onSearch"
-      @operation="onOperation"
-      @select-row="onSelectRow"
+      @search="pageProxyHandler.onSearch"
+      @operation="pageProxyHandler.onOperation"
+      @select-row="pageProxyHandler.onSelectRow"
     >
       <template #additionCommand>
         <el-button
@@ -30,8 +30,7 @@
 import { ref, Ref, watch } from "vue";
 import varColor from "@/styles/define.module.scss";
 import ListPage from "@/components/views/ListPage.vue";
-import { DialogConfig, Model } from "@/components/views/data-dialog";
-import { OperationNamed, SearchParams } from "@/components/views/list-page";
+import DataDialog from "@/components/views/DataDialog.vue";
 import {
   ModifierUser,
   initDialogData as initUserDialogData,
@@ -42,12 +41,12 @@ import {
   initDialogData as initAssignDialogData,
 } from "./config/assign-data-dialog";
 import { userListPage } from "./config/user-list-page";
+import { usePageProxyHandler } from "@/components/views/pageProxyHandler";
+import { ElMessage } from "element-plus";
+import usePermission from "@/components/login/hooks/usePermission";
 import { User, useUsersStore } from "./store/usersStore";
 import { useDepartmentStore } from "@/components/system/department/store/departmentStore";
 import { useRoleStore } from "@/components/system/role/store/roleStore";
-import DataDialog from "@/components/views/DataDialog.vue";
-import usePermission from "@/components/login/hooks/usePermission";
-import { ElMessage } from "element-plus";
 
 const userPageConfig = ref(userListPage);
 const showAssignDialog = ref(false);
@@ -56,103 +55,95 @@ const assignConfig = ref(assignDialogConfig);
 const usersStore = useUsersStore();
 const departmentStore = useDepartmentStore();
 const roleStore = useRoleStore();
-let currentSelectedUser: User;
-let tableDataRef: Ref<Model[]>;
-let reqParams: SearchParams;
 
-// init load department list
-if (!departmentStore.departmentAllTree.length) {
-  departmentStore
-    .fetchAll()
-    .then(() =>
-      initUserDialogData(userPageConfig, departmentStore.departmentAllTree)
+const pageProxyHandler = usePageProxyHandler<ModifierUser, User>({
+  init() {
+    // init load department list
+    if (!departmentStore.departmentAllTree.length) {
+      departmentStore
+        .fetchAll()
+        .then(() =>
+          initUserDialogData(userPageConfig, departmentStore.departmentAllTree)
+        );
+    }
+    if (!roleStore.rolesAllSource.length) {
+      roleStore
+        .fetchAll()
+        .then(() =>
+          initAssignDialogData(assignConfig, roleStore.rolesAllSource)
+        );
+    }
+    // set department name while id has change
+    watch(
+      () => userPageConfig.value.struct.dialogConfig?.model.departmentId,
+      (departId) => {
+        if (!departId) {
+          return;
+        }
+        // array -> number
+        if ((departId as any) instanceof Array) {
+          const ids = departId as unknown as [];
+          userPageConfig.value.struct.dialogConfig!.model.departmentId =
+            ids[ids.length - 1];
+          return;
+        }
+        userPageConfig.value.struct.dialogConfig!.model.departmentName =
+          departmentStore.findDepartmentNameById(departId)!;
+      }
     );
-}
-if (!roleStore.rolesAllSource.length) {
-  roleStore
-    .fetchAll()
-    .then(() => initAssignDialogData(assignConfig, roleStore.rolesAllSource));
-}
-// set department name while id has change
-watch(
-  () => userPageConfig.value.struct.dialogConfig?.model.departmentId,
-  (departId) => {
-    if (!departId) {
+  },
+  onSearch(searchParams, tableData) {
+    usersStore
+      .fetchPage({
+        pageStart: searchParams.searchIndex,
+        pageSize: searchParams.searchPageSize,
+        entity: {
+          username: searchParams.searchKeyName,
+          realName: searchParams.realName,
+          phone: searchParams.phone,
+        },
+      })
+      .then(() => (tableData.value = usersStore.userList));
+  },
+  onOperation(name, dialogConfig, selectedRow?) {
+    const config = dialogConfig.value;
+    if (name === "add") {
+      config.title = "添加用户";
+      config.request.url = "/api/manage/user/add";
+      config.request.method = "post";
+      config.model = <ModifierUser>{
+        gender: 0,
+        departmentId: 1,
+      };
+      if (selectedRow) {
+        const selectedUser = selectedRow as User;
+        config.model.departmentId = selectedUser.departmentId;
+      }
       return;
     }
-    // array -> number
-    if ((departId as any) instanceof Array) {
-      const ids = departId as unknown as [];
-      userPageConfig.value.struct.dialogConfig!.model.departmentId =
-        ids[ids.length - 1];
+    if (name == "edit") {
+      config.title = "修改用户";
+      config.request.url = "/api/manage/user/update";
+      config.request.method = "put";
+      const { departmentName, ...updateParams } = selectedRow as ModifierUser;
+      const autoSetDepartName = config.model.departmentName;
+      config.model = { departmentName: autoSetDepartName, ...updateParams };
       return;
     }
-    userPageConfig.value.struct.dialogConfig!.model.departmentName =
-      departmentStore.findDepartmentNameById(departId)!;
-  }
-);
-
-function onSelectRow(selectedRow: Model) {
-  currentSelectedUser = selectedRow as User;
-}
-
-function onSearch(searchParams: SearchParams, tableData: Ref<Model[]>) {
-  reqParams = searchParams;
-  tableDataRef = tableData;
-  usersStore
-    .fetchPage({
-      pageStart: searchParams.searchIndex,
-      pageSize: searchParams.searchPageSize,
-      entity: {
-        username: searchParams.searchKeyName,
-        realName: searchParams.realName,
-        phone: searchParams.phone,
-      },
-    })
-    .then(() => (tableData.value = usersStore.userList));
-}
-
-function onOperation(
-  name: OperationNamed,
-  dialogConfig: Ref<DialogConfig<Model>>,
-  selectedRow?: Model
-) {
-  const config = (dialogConfig as Ref<DialogConfig<ModifierUser>>).value;
-  if (name === "add") {
-    config.title = "添加用户";
-    config.request.url = "/api/manage/user/add";
-    config.request.method = "post";
-    config.model = <ModifierUser>{
-      gender: 0,
-      departmentId: 1,
-    };
-    if (selectedRow) {
-      const selectedUser = selectedRow as User;
-      config.model.departmentId = selectedUser.departmentId;
+    if (name == "remove") {
+      if (selectedRow!.isAdmin) {
+        ElMessage.warning("超级管理员用户不能删除！");
+        return;
+      }
+      usersStore
+        .removeRecord(selectedRow!.id)
+        .then(() => pageProxyHandler.refresh());
     }
-    return;
-  }
-  if (name == "edit") {
-    config.title = "修改用户";
-    config.request.url = "/api/manage/user/update";
-    config.request.method = "put";
-    const { departmentName, ...updateParams } = selectedRow as ModifierUser;
-    const autoSetDepartName = config.model.departmentName;
-    config.model = { departmentName: autoSetDepartName, ...updateParams };
-    return;
-  }
-  if (name == "remove") {
-    if (selectedRow!.isAdmin) {
-      ElMessage.warning("超级管理员用户不能删除！");
-      return;
-    }
-    usersStore.removeRecord(selectedRow!.id).then((_) => {
-      onSearch(reqParams, tableDataRef);
-    });
-  }
-}
+  },
+});
 
 async function onAssign() {
+  const currentSelectedUser = pageProxyHandler.getCurrentSelectedRow();
   if (!currentSelectedUser) {
     ElMessage.warning("请选择用户！");
     return;
@@ -168,7 +159,7 @@ async function onAssign() {
 function onAssignDialogClose(finish: boolean) {
   showAssignDialog.value = false;
   if (finish) {
-    onSearch(reqParams, tableDataRef);
+    pageProxyHandler.refresh();
   }
 }
 </script>
---
+++ src/components/views/pageProxyHandler.ts
@@ -0,0 +1,63 @@
+import { Ref } from "vue";
+import { DialogConfig, Model } from "./data-dialog";
+import { OperationNamed, SearchParams } from "./list-page";
+
+/**
+ * Page处理器参数（D: Dialog模型类型，R: 表记录行的类型）
+ */
+export interface PageHandlerParams<D extends Model, R> {
+  /**
+   * 初始化
+   */
+  init?: () => void;
+  /**
+   * 页搜索
+   * @param searchParams 搜索参数
+   * @param tableData 表数据引用
+   */
+  onSearch(searchParams: SearchParams, tableData: Ref<Model[]>): void;
+  /**
+   * 执行操作
+   * @param name 当前操作名
+   * @param dialogConfig dialog配置对象
+   * @param selectedRow 记录行数据
+   */
+  onOperation(
+    name: OperationNamed,
+    dialogConfig: Ref<DialogConfig<D>>,
+    selectedRow?: R
+  ): void;
+}
+
+export const usePageProxyHandler = function <D extends Model, R>(
+  params: PageHandlerParams<D, R>
+) {
+  let tableDataRef: Ref<Model[]>;
+  let reqParams: SearchParams;
+  let currentSelectedRow: R;
+
+  params.init && params.init();
+  return {
+    getCurrentSelectedRow() {
+      return currentSelectedRow;
+    },
+    refresh() {
+      this.onSearch(reqParams, tableDataRef);
+    },
+    onSelectRow(selectedRow: Model) {
+      currentSelectedRow = selectedRow as R;
+    },
+    onSearch(searchParams: SearchParams, tableData: Ref<Model[]>) {
+      reqParams = searchParams;
+      tableDataRef = tableData;
+      params.onSearch(searchParams, tableData);
+    },
+    onOperation(
+      name: OperationNamed,
+      dialogConfig: Ref<DialogConfig<D>>,
+      selectedRow?: R
+    ) {
+      params.onOperation(name, dialogConfig, selectedRow);
+    },
+  };
+};
