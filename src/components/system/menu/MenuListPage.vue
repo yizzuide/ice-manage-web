e1a01f6719080ba21bc860973dc198f0ad52acd8
yizzuide <fu837014586@163.com>
2022-11-27 03:21:26 +0800

    refactor(menu): refactor menu with page proxy handler

--- src/components/system/menu/MenuListPage.vue
+++ src/components/system/menu/MenuListPage.vue
@@ -3,79 +3,70 @@
     :page="menuListPage"
     :total="menuStore.totalSize"
     :page-count="menuStore.pageCount"
-    @search="onSearch"
-    @operation="onOperation"
+    @search="pageProxyHandler.onSearch"
+    @operation="pageProxyHandler.onOperation"
   ></ListPage>
 </template>
 
 <script setup lang="ts">
-import { Ref } from "vue";
-import { DialogConfig, Model } from "@/components/views/data-dialog";
-import { OperationNamed, SearchParams } from "@/components/views/list-page";
+import { ElMessage } from "element-plus";
 import ListPage from "@/components/views/ListPage.vue";
 import { menuListPage } from "./config/menu-list-page";
-import { Menu, useMenuStore } from "./store/menuStore";
 import { ModifierMenu } from "./config/menu-data-dialog";
-import { ElMessage } from "element-plus";
+import { Menu, useMenuStore } from "./store/menuStore";
+import { usePageProxyHandler } from "@/components/views/pageProxyHandler";
 
 const menuStore = useMenuStore();
-let tableDataRef: Ref<Model[]>;
-let reqParams: SearchParams;
 
-function onSearch(searchParams: SearchParams, tableData: Ref<Model[]>) {
-  reqParams = searchParams;
-  tableDataRef = tableData;
-  menuStore
-    .fetchPage({
-      pageStart: searchParams.searchIndex,
-      pageSize: searchParams.searchPageSize,
-      startDate: searchParams.searchDate?.[0],
-      endDate: searchParams.searchDate?.[1],
-      entity: {
-        label: searchParams.searchKeyName,
-      },
-    })
-    .then(() => (tableData.value = menuStore.menuList));
-}
-
-function onOperation(
-  name: OperationNamed,
-  dialogConfig: Ref<DialogConfig<Model>>,
-  selectedRow?: Model
-) {
-  const config = (dialogConfig as Ref<DialogConfig<ModifierMenu>>).value;
-  if (name === "add") {
-    config.title = "添加菜单";
-    config.request.url = "/api/manage/menu/add";
-    config.model = <Menu>{};
-    config.model.order = 0;
-    (config.model.parentId = 0), (config.model.parentName = "");
-    if (selectedRow) {
-      config.model.parentName = selectedRow.label;
-      config.model.parentId = selectedRow.id;
-    }
-    return;
-  }
-  if (name == "edit") {
-    config.title = "修改菜单";
-    config.request.url = "/api/manage/menu/update";
-    config.request.method = "put";
-    // 复制对象属性过滤之：解构剩余参数（适用于排除的参数小于5个）
-    const { parentId, parentName, createTime, children, ...updateParams } =
-      selectedRow as Menu;
-    config.model = updateParams;
-    return;
-  }
-  if (name == "remove") {
-    menuStore.removeRecord(selectedRow!.id).then((data) => {
-      if (!data.isSuccess) {
-        ElMessage.error(data.message);
-        return;
+const pageProxyHandler = usePageProxyHandler<ModifierMenu, Menu>({
+  onSearch(searchParams, tableData) {
+    menuStore
+      .fetchPage({
+        pageStart: searchParams.searchIndex,
+        pageSize: searchParams.searchPageSize,
+        startDate: searchParams.searchDate?.[0],
+        endDate: searchParams.searchDate?.[1],
+        entity: {
+          label: searchParams.searchKeyName,
+        },
+      })
+      .then(() => (tableData.value = menuStore.menuList));
+  },
+  onOperation(name, dialogConfig, selectedRow?) {
+    const config = dialogConfig.value;
+    if (name === "add") {
+      config.title = "添加菜单";
+      config.request.url = "/api/manage/menu/add";
+      config.model = <Menu>{};
+      config.model.order = 0;
+      (config.model.parentId = 0), (config.model.parentName = "");
+      if (selectedRow) {
+        config.model.parentName = selectedRow.label;
+        config.model.parentId = selectedRow.id;
       }
-      onSearch(reqParams, tableDataRef);
-    });
-  }
-}
+      return;
+    }
+    if (name == "edit") {
+      config.title = "修改菜单";
+      config.request.url = "/api/manage/menu/update";
+      config.request.method = "put";
+      // 复制对象属性过滤之：解构剩余参数（适用于排除的参数小于5个）
+      const { parentId, parentName, createTime, children, ...updateParams } =
+        selectedRow as Menu;
+      config.model = updateParams;
+      return;
+    }
+    if (name == "remove") {
+      menuStore.removeRecord(selectedRow!.id).then((data) => {
+        if (!data.isSuccess) {
+          ElMessage.error(data.message);
+          return;
+        }
+        pageProxyHandler.refresh();
+      });
+    }
+  },
+});
 </script>
 
 <style scoped></style>
